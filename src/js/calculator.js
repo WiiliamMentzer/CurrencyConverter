676fc6e306679b60e133090dd2ed0c99cf817718
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-09-19 03:51:34 -0700

    WIP complete rework of project API calling

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,25 +1,54 @@
+import { printElements } from ".";
 
-export function currencyConversionRates(currency1, currency2) {
-  let curPromise = new Promise(function(resolve, reject){
-    let apiRequest = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${cur}`;
+export function currencyConversionRates(currency1) {
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency1}`;
 
-    apiRequest.addEventListener("loadend", function(){
-      const apiRespone = JSON.parse(this.responseText);
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, currency1);
+    }
+  });
 
-      if(this.status === 200){
-        resolve(apiRespone);
-      } else {
-        reject(this);
-      }
-    })
-    apiRequest.open("GET", url, true);
-    apiRequest.send();
-  })
-
-  curPromise.then(function(apiRespone) {
-    curResult(apiRespone);
-  }, function(errorMSG) {
-    curError(errorMSG);
-  })
+  request.open("GET", url, true);
+  request.send();
 }
+//   let curPromise = new Promise(function(resolve, reject){
+//     let apiRequest = new XMLHttpRequest();
+//     if (currency2 === null) {
+//       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${cur}`;
+
+//       apiRequest.addEventListener("loadend", function(){
+//         const apiRespone = JSON.parse(this.responseText);
+
+//         if(this.status === 200){
+//           resolve(apiRespone);
+//         } else {
+//           reject(this);
+//         }
+//         apiRequest.open("GET", url, true);
+//         apiRequest.send();
+//       })
+//     } else {
+//       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/AMOUNT`;
+
+//       apiRequest.addEventListener("loadend", function(){
+//         const apiRespone = JSON.parse(this.responseText);
+
+//         if(this.status === 200){
+//           resolve(apiRespone);
+//         } else {
+//           reject(this);
+//         }
+//         apiRequest.open("GET", url, true);
+//         apiRequest.send();
+//       })
+//     }
+
+//   curPromise.then(function(apiRespone) {
+//     curResult(apiRespone);
+//   }, function(errorMSG) {
+//     curError(errorMSG);
+//   })
+// }
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,26 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min,css'
-import './css/styles.css'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min,css'
+// import './css/styles.css'
 import { currencyConversionRates } from './calculator';
 
-export function currencyResult(apiRespone){
-  document.getElementById("CurrencyConverted").innerHTML = null;
-  const curDisplay = document.getElementById("CurrencyConverted");
+
+export function printElements(apiResponse, currency1) {
+  document.getElementById('CurrencyConverted').innerText =  `${currency1} ${apiResponse.conversion_rates.currency1}%.`;
 }
+
+// export function currencyResult(apiRespone){
+//   document.getElementById("CurrencyConverted").innerHTML = null;
+//   const curDisplay = document.getElementById("CurrencyConverted");
+// }
 
 function formSubmit(event){
-  event.preventDefauly();
+  event.preventDefault();
 
-  let currency1 = document.getElementById("CurrencySelector1").value;
-  let currency2 = document.getElementById("CurrencySelector2").value;
-  currencyConversionRates(currency1, currency2);
+  let currency1 = "USD";
+  // let currency2 = document.getElementById("CurrencySelector2").value;
+  currencyConversionRates(currency1);
 }
+
+window.addEventListener("load", function(){
+	document.getElementById("user-input").addEventListener("submit", formSubmit);
+});
